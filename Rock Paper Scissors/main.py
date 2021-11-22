9d6cd280499c27712857129a40073020a1ea70b1
LoGinto <46216287+LoGinto@users.noreply.github.com>
2021-11-21 20:19:51 -0800

    My first commit

---
+++ Rock Paper Scissors/main.py
@@ -0,0 +1,22 @@
+import random
+
+
+def play():
+    user = input("'r' for rock,'p' for paper,'s' for scissors ")
+    computer = random.choice(['r', 'p', 's'])
+    if user == computer:
+        return 'tie'
+    if isWin(user, computer):
+        return 'You won'
+    print("computer had "+computer)
+    return 'you lost'
+
+
+def isWin(player, opponent):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (
+            player == 'p' and opponent == 'r'):
+        print("opponent had a "+opponent)
+        return True
+
+
+print(play())
