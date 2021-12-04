86fdc6722191be2828a4644e5a5432404ffb3278
LoGinto <46216287+LoGinto@users.noreply.github.com>
2021-12-03 18:08:51 -0800

    Clock

---
+++ Clock/main.py
@@ -0,0 +1,18 @@
+import tkinter
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+root = Tk()
+root.title("Clock")
+
+
+label = Label(root, font = ("Helvetica"),background = "black",foreground = "cyan")
+label.pack(anchor = 'center')
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000, time)
+
+time()
+mainloop()
